fc7227e52aed94044ee1b08d612f5c5650b7d6a7
slince <taosikai@yeah.net>
2016-05-15 20:55:35 +0800

    初次提交

--- services/PrintOrder.php
+++ services/PrintOrder.php
@@ -6,9 +6,5 @@ class PrintOrder extends AbstractService
 
     function perform($arguments = array())
     {
-        global $db;
-        $billno = strval($arguments['billno']);
-        $order = $db->getRow("select * from rs_order where billno = '{$billno}' limit 1");
-        $this->log(print_r($order, true));
     }
 }
